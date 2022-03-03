37a4b673133fbb0cecb2a93a2e6d68fb1406dded
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2022-03-02 18:57:43 -0800

    Create bitmap_experiments.ino

---
+++ bitmap_experiments.ino
@@ -0,0 +1,64 @@
+/*
+
+Example for ESP_8_BIT color composite video generator library on ESP32.
+Connect GPIO25 to signal line, usually the center of composite video plug.
+
+GFX Hello World
+
+This demonstrates using the ESP_8_BIT_GFX class, which inherits from the
+Adafruit GFX base class to deliver an easy to use graphics API. Draws two
+rectangles that cycle around the border of the screen. The amount of corners
+cut off from these rectangle show the amount of overscan on a particular
+screen. In the middle of two rectangles are a bit of text drawn using
+Adafruit GFX print() API.
+
+Copyright (c) Roger Cheng
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include <ESP_8_BIT_GFX.h>
+#include "speaker.h"
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+
+
+// Create an instance of the graphics library
+ESP_8_BIT_GFX videoOut(true /* = NTSC */, 8 /* = RGB332 color */);
+
+
+void setup() {
+  // Initial setup of graphics library
+  videoOut.begin();
+}
+
+
+
+void loop() {
+  // Clear screen
+  videoOut.fillScreen(0);
+  videoOut.drawBitmap(125,125,speaker, 44,33,1);
+  // Draw one rectangle
+
+ 
+}
