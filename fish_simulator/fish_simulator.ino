5105e475966286ce94b5cd0e3fcdc812dd2a14b6
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2022-03-12 14:19:23 -0800

    Create fish_simulator.ino

---
+++ fish_simulator/fish_simulator.ino
@@ -0,0 +1,52 @@
+#include <ESP_8_BIT_GFX.h>
+#include "Fish.h"
+#include "Fish2.h"
+#include "Fish3.h"
+#include "seaweeds.h"
+#include "bubbles.h"
+
+// Create an instance of the graphics library
+ESP_8_BIT_GFX videoOut(true /* = NTSC */, 8 /* = RGB332 color */);
+
+bubble oneBubble(random(0,255), 239, 1, 0xE8, 6);
+bubble twoBubble(random(0,255), 239, 1.3, 0xE8, 8);
+bubble threeBubble(random(0,255), 239, .7, 0xE8, 5);
+
+circle_fish mudfish(100,120,.7,0xE8, 0.05, 3);
+
+circle_fish2 shark(100,120,.5,0xE8, 0.03, 3.5);
+
+circle_fish3 roundfish(100,120,.3,0xE8, 0.03, 2);
+
+//bubbles Bubble(120, 120, 1, 0xE8);
+
+void setup() {
+  // Initial setup of graphics library
+  videoOut.begin();
+  videoOut.setRotation(3);
+}
+
+void loop() {
+  // Wait for the next frame to start working on frame buffer
+  videoOut.waitForFrame();
+
+  // Clear screen
+  videoOut.fillScreen(0);
+
+  oneBubble.draw(videoOut);
+  twoBubble.draw(videoOut);
+  threeBubble.draw(videoOut);
+
+
+  mudfish.draw(videoOut);
+
+  shark.draw(videoOut);
+
+  roundfish.draw(videoOut);
+
+
+  videoOut.drawBitmap(10, 15,  epd_bitmap_leftseaweed, 31, 221, 0xE8);
+  videoOut.drawBitmap(210, 13,  epd_bitmap_leftseaweed, 32, 221, 0xE8);
+
+
+}
