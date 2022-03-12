36dba6bea856b8f154995ea30e52a9c1c2f5f3e0
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2022-03-12 08:18:44 -0800

    Create Fish2.h

---
+++ Fish2.h
@@ -0,0 +1,37 @@
+#include <ESP_8_BIT_GFX.h>
+float incrementer2;
+float fishWiggle2;
+float wiggleAmount2;
+// Fish
+class circle_fish2
+{
+  public:
+    circle_fish2(float start_x, float start_y, float start_deltaX, int start_color, float wiggleValue, float wiggleHeight)
+    {
+      x = start_x;
+      y = start_y;
+      deltaX = start_deltaX;
+      color = start_color;
+      fishWiggle2 = wiggleValue;
+      wiggleAmount2 = wiggleHeight;
+    }
+   
+    void draw(ESP_8_BIT_GFX gfx)
+    {
+      gfx.drawCircle(x, y, 10, color);
+     if(x>0){ 
+      x -= deltaX;
+      y-=(sin(incrementer2)/wiggleAmount);
+      incrementer2+=fishWiggle2;
+     }
+       else{
+    x=255;
+    y=random(20,219);
+    incrementer2=0;
+  }
+    }
+
+  private:
+    float x, y, deltaX;
+    int color;
+};
