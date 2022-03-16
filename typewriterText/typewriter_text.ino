e84317606de2e2350de29cfd1efeb536e29b1bc7
emilyvelasco <54129995+emilyvelasco@users.noreply.github.com>
2022-03-16 08:50:13 -0700

    Create typewriter_text.ino

---
+++ typewriterText/typewriter_text.ino
@@ -0,0 +1,36 @@
+int row = 0;
+int column = 0;
+
+char *textBlock[]={"This is line 1","This is line 2","This is line 3",
+  "This is line 4","This is line 5"};
+  char *printedLine;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+
+  printedLine = textBlock[row];
+    if (column< 14){
+       Serial.print(printedLine[column]);
+       column++;
+       delay(100);
+    }
+    else if(column >=14){
+      Serial.println();
+      row++;
+      column=0;
+    }
+    else{
+      row = 0;
+      column = 0;
+      delay(1000);
+    }
+
+
+}
